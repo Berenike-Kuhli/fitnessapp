db992239aec7b3fa7bc9a4516656cceff5dfbeb9
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-04-25 22:07:09 +0200

    first input for dashboard

---
+++ src/App/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App h-screen bg-dark">
+      <div>
+        <h1 className="text-4xl font-bold text-light">Hi Name!</h1>
+<img src="../startbild.png"></img>
+        <h3 className="text-2xl font-bold text-light">Dein Workout heute</h3>
+        <label className="text-xs font-normal text-light">Trainingsplan</label>
+      </div>
+      <div>
+        <h3 className="text-2xl font-bold text-light">Tag 2</h3>
+        <h2 className="text-3xl font-bold text-light">Titel des Programms</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
