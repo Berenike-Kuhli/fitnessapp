0cc780ca695af00b09daf61d25686fd4fd37cc65
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-04-27 12:38:36 +0200

    add content to dashboard

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,14 +5,22 @@ function App() {
     <div className="App h-screen bg-dark">
       <div>
         <h1 className="text-4xl font-bold text-light">Hi Name!</h1>
-<img src="../startbild.png"></img>
+        <img src="../startbild.png"></img>
         <h3 className="text-2xl font-bold text-light">Dein Workout heute</h3>
         <label className="text-xs font-normal text-light">Trainingsplan</label>
       </div>
-      <div>
+      <div classname="bg-medium">
         <h3 className="text-2xl font-bold text-light">Tag 2</h3>
         <h2 className="text-3xl font-bold text-light">Titel des Programms</h2>
+        <label className="text-xs font-normal text-light">26 Min.</label>
+        <label className="text-xs font-normal text-light"> &middot; </label>
+        <label className="text-xs font-normal text-light">Beweglichkeit</label>
       </div>
+      <nav>
+        <img src="./icon-house.svg"></img>
+        <img src="./icon-dumbbell.svg"></img>
+        <img src="./icon-avatar.svg"></img>
+      </nav>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,14 @@
 module.exports = {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      'light': '#FBFBFB',
-      'medium': '#3A4151',
-      'dark': '#202430'
+    
+    extend: {
+      colors: {
+        'light': '#FBFBFB',
+        'medium': '#3A4151',
+        'dark': '#202430'
+      },
     },
-   
-//     bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500
-//     bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-pink-500 via-red-500 to-yellow-500
-// ,
-//     fontFamily: {
-//       sans: ['Poppins', 'sans-serif'],
-//     },
-//     extend: {
-//       backgroundImage: (theme) => ({
-//         'gradient-one': `linear-gradient(to right, ${theme('colors.pink')}, ${theme('colors.orange')})`,
-//       }),
-//       {
-//       (theme) => ({
-//           'gradient-two': `linear-gradient(to right, ${theme('colors.pink')}, ${theme('colors.orange')})`,
-//         }),
-//       },
-//       {
-// (theme) => ({
-//           'gradient-three': `linear-gradient(to right, ${theme('colors.pink')}, ${theme('colors.orange')})`,
-//         }),
-//       },
-//     },
   },
   plugins: [],
 };
