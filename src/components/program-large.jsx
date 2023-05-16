a1c53210d00e9f428b5f1b127b944ff9f5b08ab7
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-05-16 14:32:55 +0200

    started component for programs

---
+++ src/components/program-large.jsx
@@ -0,0 +1,12 @@
+import { ButtonLight } from "./buttons/button-primary";
+
+function ProgramLarge () {
+ return (
+<div>
+    <p>Vorschau</p>
+<ButtonLight><p>Programm auswählen</p></ButtonLight>/>
+</div>
+ );   
+}
+
+export default ProgramLarge;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import ErrorPage from "./pages/error-page";
 import {
   ApolloClient,
