d8e244cb4679dbe2f0c925f95b2ff828a5366bdd
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-05-17 20:12:02 +0200

    update einbindung icons und bilder

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,10 +1,10 @@
-import NavBar from "../components/NavBar";
+
+// das className gebe ich mit (und rufe es mit ${} auf, um zusätzlich zu den definierten Sachen noch eigene beim Aufruf der Component mitgeben zu können)
 
 function DefaultLayout({ children, className }) {
   return (
     <div className={`w-100 ${className} flex min-h-screen flex-col bg-dark`}>
       {children}
-      <NavBar />
     </div>
   );
 }
