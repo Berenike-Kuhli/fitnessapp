7a98dde09400fff0e59b986cadce544fb8e255fa
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-05-24 22:14:21 +0200

    add component workout, not finished yet

---
+++ src/components/Workout.jsx
@@ -0,0 +1,14 @@
+export default function Workout () {
+    return (
+    <div className="w-96 h-24 bg-medium rounded-[20px] m-3.5">
+       
+<div className="w-24 h-24 bg-light rounded-l-[20px]"></div>
+<div>
+<h1 className="text-lg text-light font-bold">Tag 1</h1>
+<p className="text-light text-xs">26 Min. &middot;</p>
+<p className="text-light text-xs">Beweglichkeit</p>
+</div>
+    </div>
+
+    );
+}
--- src/pages/Sandbox.jsx
+++ src/pages/Sandbox.jsx
@@ -1,6 +1,7 @@
 import InputField from "../components/InputField";
 import DefaultLayout from "../layouts/DefaultLayout";
 import Square from "../components/Square";
+import Workout from "../components/Workout";
 
 
 export default function Sandbox () {
@@ -8,6 +9,7 @@ export default function Sandbox () {
         <DefaultLayout>
             <InputField/>
             <Square/>
+            <Workout/>
         </DefaultLayout>
     );
 }
