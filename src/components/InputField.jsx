04341a94c654a40526b1d08afb9e78940909ea21
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-05-24 21:55:02 +0200

    working on InputField to make is more flexibel, not done yet

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,8 +1,11 @@
 //TODO wie den placeholder flexibel gestalten
 
-export default function InputField (placeholder) {
-    const placeholder = {};
+export default function InputField (props) {
+    const placeholder = "test"
     return(
-        <input type="text" placeholder= {$placeholder} className="rounded-xl h-11 w-[19rem] border border-light bg-transparent text-light py-3 pl-6"/>
+        <input type="text" 
+        placeholder={placeholder} 
+        // placeholder= {props.text} 
+        className="rounded-xl h-11 w-[19rem] border border-light bg-transparent text-light py-3 pl-6"/>
     );
 }
