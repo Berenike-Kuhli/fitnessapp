8e1e1a34266b0efc533234c97607f05b1b99c5b3
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-05-25 13:15:16 +0200

    solve the Aufrufproblem for gradients

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,12 +1,10 @@
 //TODO wie den placeholder flexibel gestalten
 
-export default function InputField(props) {
-  const placeholder = "test";
+export default function InputField({placeholder}) {
   return (
     <input
       type="text"
       placeholder={placeholder}
-      // placeholder= {props.text}
       className="m-3.5 h-11 w-[19rem] rounded-xl border border-light bg-transparent py-3 pl-6 text-light"
     />
   );
--- src/pages/Sandbox.jsx
+++ src/pages/Sandbox.jsx
@@ -2,14 +2,17 @@ import InputField from "../components/InputField";
 import DefaultLayout from "../layouts/DefaultLayout";
 import Square from "../components/Square";
 import Workout from "../components/Workout";
+import ButtonPrimary from "../components/buttons/ButtonPrimary";
 
 
 export default function Sandbox () {
     return (
         <DefaultLayout>
-            <InputField/>
+            <InputField placeholder={"Test4"}/>
             <Square/>
             <Workout/>
+            <ButtonPrimary bg="grad2">Test</ButtonPrimary>
         </DefaultLayout>
+        
     );
 }
