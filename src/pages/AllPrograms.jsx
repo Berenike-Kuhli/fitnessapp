665e2cbc782a21ae061bed25bd8e933ffbdd1fb8
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-05-27 13:06:29 +0200

    connects database with program rectangle

--- src/pages/AllPrograms.jsx
+++ src/pages/AllPrograms.jsx
@@ -4,7 +4,6 @@ import { gql, useQuery } from "@apollo/client";
 
 // TODO Es werden nicht alle Programme direkt geladen. Entweder baut ihr ein endless scrolling wo ab einer bestimmten Scrollposition neue Programme geladen werden oder einen "weitere laden" Button unter den jeweils schon angezeigten Programmen
 
-// TODO get this working
 const GET_PROGRAMS = gql`
   query GetPrograms {
     programs {
@@ -12,26 +11,23 @@ const GET_PROGRAMS = gql`
     }
   }`;
 
-  function Programs({ GetPrograms }) {
-    const { loading, error, data } = useQuery(query);
-  
-    if (loading) return null;
-    if (error) return `Error! ${error}`;
-  
-    return <pre>{JSON.stringify(data, null, 2)}</pre>;
-  }
-
 
 export default function AllPrograms() {
+  const { loading, error, data } = useQuery(GET_PROGRAMS);
+  
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+  
+
   return (
     <DefaultLayout>
       <h2 className="py-14 pl-4 text-left text-3xl font-bold text-light">
         Browse
       </h2>
       {/* TODO make gradients alternate */}
+      {/* TODO geht all the programs without hardcoding it here */}
       <ProgramMedium>
-        {/* TODO add data from GraphQL */}
-        <p>Test</p>
+        <p>{data.programs[0].name}</p>
       </ProgramMedium>
       <ProgramMedium />
       <ProgramMedium />
